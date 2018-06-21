fe94a3febc27ad505acf59091860c4f7b4661544
Gabriel <gmfiga@gmail.com>
2018-06-21 14:02:57 -0400

    done

--- ImageRandomizer/ViewController.swift
+++ ImageRandomizer/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var btnNext: UIButton!
+    var id:Int = 0
+    var randomNum:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changePic(_ sender: Any) {
+        while (randomNum == id) {
+            randomNum = Int(arc4random_uniform(9))+1
+        }
+        
+        id = randomNum
+        let imageName = "logo\(randomNum).png"
+        mainImage.image = UIImage(named: imageName)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
+        }
 }
+
 
